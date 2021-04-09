d2250d6512145cfa5ee1c466ad28b4cba9dfe94a
Jasper <j.valdivia@returngenerics.com>
2021-04-09 11:55:55 -0400

    Boilerplate
    Boilerplate Code for Testing Environment

--- Radagast/ViewControllers/MainViewController.swift
+++ Radagast/ViewControllers/MainViewController.swift
@@ -8,10 +8,21 @@
 import UIKit
 
 class MainViewController: UIViewController {
+    
+    let carbonTable = CarbonTableViewController()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        self.view.backgroundColor = .systemGray
+        carbonTable.view.backgroundColor = .systemPink
+        self.navigationController?.navigationBar.isHidden = true
+        self.add(carbonTable, constraints: [
+            carbonTable.view.heightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.heightAnchor, multiplier: 0.8),
+            carbonTable.view.widthAnchor.constraint(equalTo: view.safeAreaLayoutGuide.widthAnchor, multiplier: 0.8),
+            carbonTable.view.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            carbonTable.view.centerXAnchor.constraint(equalTo: view.centerXAnchor)
+        ])
     }
 
 
