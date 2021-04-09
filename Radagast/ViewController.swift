b84c84a6a7855976940dfa322e991f5481c3ff71
jasval <j.valdivia@returngenerics.com>
2021-04-09 09:34:25 -0400

    First Commit [Starting Radagast]
    This commit contains setup Xcode code.

---
+++ Radagast/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Radagast
+//
+//  Created by Jasper Valdivia on 09/04/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
